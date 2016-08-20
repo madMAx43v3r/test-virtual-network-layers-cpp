d089ace2d5d181e7848c4b0450bdf7b97f0006c2
Max Wittal <max.wittal@mwittal.de>
2016-08-20 01:09:14 -0700

    changed library names

--- codegen.sh
+++ codegen.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 
-cd lib/virtual-network-interface/codegen/cpp
-cmake .
-make
-cd ../../../../
-
-lib/virtual-network-interface/codegen/cpp/vnicppcodegen lib/virtual-network-layers-cpp/interface interface/
+vnicppcodegen lib/virtual-network-layers-cpp/interface interface/
 
